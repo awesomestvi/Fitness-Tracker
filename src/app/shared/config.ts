04af57dd02447bd5eaf874ef482f3f4a4e4c7624
awesomestvi <vishal.cn.92@gmail.com>
2021-09-07 04:04:08 +0200

    Fixing CSS for mobile screens

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -3,5 +3,5 @@ const BASE_URL = "https://identitytoolkit.googleapis.com/v1/accounts:";
 
 export const LOGIN_URL = `${BASE_URL}signInWithPassword?key=${API_KEY}`;
 export const SIGNUP_URL = `${BASE_URL}signUp?key=${API_KEY}`;
-// export const API_ENDPOINT = "https://vishals-fitness-tracker-api.herokuapp.com";
-export const API_ENDPOINT = "http://localhost:9000";
+export const API_ENDPOINT = "https://vishals-fitness-tracker-api.herokuapp.com";
+// export const API_ENDPOINT = "http://localhost:9000";
